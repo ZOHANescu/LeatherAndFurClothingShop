2fe3efd7e5941cccfa6a9f70ed9e1210b77907a3
Stefan <stefanhera@gmail.com>
2022-04-10 13:13:29 +0300

    Initial commit with registration feature and Nitrite Database integration.

---
+++ src/main/java/org/loose/fis/lfcs/controllers/RegistrationController.java
@@ -0,0 +1,36 @@
+package org.loose.fis.lfcs.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.loose.fis.lfcs.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.lfcs.services.UserService;
+
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Customer", "Admin");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+}
