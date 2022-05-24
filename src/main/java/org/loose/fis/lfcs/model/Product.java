8f7941ccb0747e2c60bdb5e0d4218dc484b4450c
Stefan <stefanhera@gmail.com>
2022-05-24 03:41:40 +0300

    LFCS-5 Put the lombok getter and setter for fewer lines in Product class.

--- src/main/java/org/loose/fis/lfcs/model/Product.java
+++ src/main/java/org/loose/fis/lfcs/model/Product.java
@@ -3,33 +3,11 @@ package org.loose.fis.lfcs.model;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class Product {
 
     private String productName;
     private double productPrice;
     private String productImgSrcPath;
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public double getProductPrice() {
-        return productPrice;
-    }
-
-    public String getProductImgSrcPath() {
-        return productImgSrcPath;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public void setProductPrice(double productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public void setProductImgSrcPath(String productImgSrcPath) {
-        this.productImgSrcPath = productImgSrcPath;
-    }
 }
